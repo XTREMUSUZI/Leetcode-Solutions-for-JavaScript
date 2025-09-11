2788fdd9d2516c21799c18666d20f1fcb3c0ab19
XTREMUSUZI <161846948+XTREMUSUZI@users.noreply.github.com>
2025-09-11 20:10:33 +0530

    Update PalindromeNumberSol.js

--- PalindromeNumberSol.js
+++ PalindromeNumberSol.js
@@ -1,14 +1,13 @@
 //In this solution, I used Math.floor function to help check if the number is a palindrome or not
 
 function isPalindrome(x) {
-        let org=x;
         let rev=0;
-        for (let i=org;i>0;i=Math.floor(i/10)){
+        for (let i=x;i>0;i=Math.floor(i/10)){
             rev=rev*10+i%10;
         }
         if (x===rev){
             return true;
         }else{
             return false;
-    }
+        }
 };
